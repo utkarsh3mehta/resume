629136572917fd6860a3f151a7ed9c3a56d63a6a
Utkarsh Mehta <umteappdev@gmail.com>
2022-03-24 21:37:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function (ev) {
+  let landing = document.getElementById("landing");
+  setTimeout(function () {
+    landing.style.zIndex = 1;
+  }, 2000); // TODO: change it to 2000
+};
