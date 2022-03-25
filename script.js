a85e5f447f4bd1f997d77932103e3c256ea2bacb
Utkarsh Mehta <umteappdev@gmail.com>
2022-03-25 12:19:00 +0530

    work and application content added

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ window.onload = function (ev) {
     landing.style.zIndex = 1;
   }, 2000); // TODO: change it to 2000
 };
+
+let anchorTags = document.querySelectorAll("a");
+anchorTags.forEach((a) => {
+  if (a.getAttribute("href").match(/#*/)) {
+    // do nothing
+  } else a.setAttribute("target", "_blank");
+});
